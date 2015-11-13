23ceb0803ccaef1223bafc774b0e3aef1a86f71a
starstone <chenglei_ios@163.com>
2015-11-13 18:53:05 +0800

    提交代码

--- ADCarousel/CLADCarousel.h
+++ ADCarousel/CLADCarousel.h
@@ -5,6 +5,12 @@
 //  Created by aayongche on 15/11/13.
 //  Copyright © 2015年 aayongche. All rights reserved.
 //
+/**
+ *  屏幕大小
+ */
+#define SCREEN_SIZE [UIScreen mainScreen].bounds.size
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 
 #import <UIKit/UIKit.h>
 typedef void(^imageClickBlock)(NSInteger index);
@@ -21,8 +27,26 @@ typedef void(^imageClickBlock)(NSInteger index);
 /**
  *  轮播的时间
  */
-@property (nonatomic, assign) CGFloat *time;
+@property (nonatomic, assign) CGFloat time;
 
+/**
+ *  创建一个CLADCarousel的实例
+ *
+ *  @param frame      坐标
+ *  @param imageArray 对应的图片
+ *  @param clickBlock 在页面的点击事件
+ *
+ *  @return 返回一个CLADCarousel的实例
+ */
++ (instancetype)scrollviewWithFrame:(CGRect)frame imageArr:(NSArray *)imageArray AndImageClickBlock:(imageClickBlock)clickBlock;
+/**
+ *  开始定时器
+ */
+- (void)beginTimer;
+/**
+ *  结束定时器
+ */
+- (void)stopTimer;
 
 
 @end
--- ADCarousel/CLADCarousel.m
+++ ADCarousel/CLADCarousel.m
@@ -8,6 +8,24 @@
 
 #import "CLADCarousel.h"
 
+@interface CLADCarousel ()<UIScrollViewDelegate>
+/**
+ *  传过来的图片数组
+ */
+@property (nonatomic, strong) NSArray *imageArr;
+/**
+ *  定时器
+ */
+@property (nonatomic, strong) NSTimer *timer;
+/**
+ *  按钮点击事件
+ */
+@property (nonatomic, assign) imageClickBlock clickBlock;
+
+
+
+@end
+
 @implementation CLADCarousel
 
 /*
@@ -17,6 +35,40 @@
     // Drawing code
 }
 */
+
++ (instancetype)scrollviewWithFrame:(CGRect)frame imageArr:(NSArray *)imageArray AndImageClickBlock:(imageClickBlock)clickBlock{
+    return [[CLADCarousel alloc] initWithFrame:frame imageArr:imageArray AndImageClickBlock:clickBlock];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame imageArr:(NSArray *)imageArray AndImageClickBlock:(imageClickBlock)clickBlock{
+    if (self = [super initWithFrame:frame]) {
+        //初始化scrollview
+        _scrollView = [[UIScrollView alloc] initWithFrame:frame];
+        _scrollView.delegate = self;
+        //设置显示当前图片
+        [_scrollView setContentOffset:CGPointMake(SCREEN_WIDTH, 0) animated:NO];
+        _scrollView.pagingEnabled = YES;
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.bounces = NO;
+        [self addSubview:_scrollView];
+        
+        //初始化pageControl
+        _pageControl = [[UIPageControl alloc] init];
+        //注意此方法可以根据页数返回UIPageControl合适的大小
+        CGSize size= [_pageControl sizeForNumberOfPages:imageArray.count];
+        _pageControl.bounds = CGRectMake(0, 0, size.width, size.height);
+        _pageControl.center = CGPointMake(SCREEN_WIDTH/2, frame.size.height-30);
+        //设置颜色
+        _pageControl.pageIndicatorTintColor = [UIColor colorWithRed:193/255.0 green:219/255.0 blue:249/255.0 alpha:1];
+        //设置当前页颜色
+        _pageControl.currentPageIndicatorTintColor = [UIColor colorWithRed:0 green:150/255.0 blue:1 alpha:1];
+        //设置总页数
+        _pageControl.numberOfPages = imageArray.count;
+        [self addSubview:_pageControl];
+        self.time = 3;
+    }
+    return self;
+}
 
 
 
